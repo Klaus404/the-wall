f55a1f308ec31d14ac87e2ee34ce74670a6cd23c
Klaus404 <barbuclaudiu404@gmail.com>
2021-09-01 14:36:33 +0300

    Am reusit sa fac API-ul de la VirusTotal sa functioneze

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 import os
 import glob
 import shutil
-from virus_total_apis import PublicApi as VirusTotalPublicApi
-import virustotal3
+import json
+import magic
+import vtapi3
+from vtapi3 import VirusTotalAPIFiles, VirusTotalAPIError
 import urllib
 import re
 
@@ -13,7 +15,22 @@ import re
 virustotal_apikey = "8585f51b40008950b8cc0a9776996697de63c73b51ac6688bfcdd84c03558329"
 download_path = "/home/klaus/Downloads"
 malware_list = "https://dasmalwerk.eu/"
-vt = VirusTotalPublicApi(virustotal_apikey)
+vt_files = VirusTotalAPIFiles(virustotal_apikey)
+
+
+def scan_file(file_path):
+    try:
+        result = vt_files.upload(str(file_path))
+    except VirusTotalAPIError as err:
+        print(err, err.err_code)
+    else:
+        if vt_files.get_last_http_error() == vt_files.HTTP_OK:
+            result = json.loads(result)
+            result = json.dumps(result, sort_keys=False, indent=4)
+            print(result)
+        else:
+            print('HTTP Error [' + str(vt_files.get_last_http_error()) + ']')
+
 
 def remove_virused_file(virused):
     decision = input(f"A fost gasit un fisier periculos:{virused}. Doriti sa l eliminati?(y/N)").lower()
@@ -31,10 +48,14 @@ def remove_virused_file(virused):
         remove_virused_file(virused)
 
 
-def search_for_virus_in_filename():
+def search_for_viruses():
     for virused in glob.glob(f'{download_path}/*'):
         if "virus" in virused or "malware" in virused:
-            remove_virused_file(virused)
+            try:
+                print(str(magic.from_file(virused)))
+                scan_file(virused)
+            except os.error as error:
+                pass
 
 
-search_for_virus_in_filename()
+search_for_viruses()
